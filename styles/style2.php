0ec8dd8e666a61306526a1412541748de0d02906
Kimberly Pash <kimberly@e-rehab.com>
2024-04-24 20:40:02 -0700

    initial file add

---
+++ styles/style2.php
@@ -0,0 +1,99 @@
+<style>
+#g-rev-style-2.uk-slider {
+    margin: 20px 10px;
+}
+
+#g-rev-style-2 .tm-review-box {
+  padding: 20px;
+  box-shadow: 0px 0px 8px #cccccc;
+  margin: 12px;
+  border-radius: 10px;
+}
+
+#g-rev-style-2 .tm-review-box span > img {
+  width: 35px;
+  height: 35px;
+  margin-bottom: 15px;
+}
+
+#g-rev-style-2 .tm-review-stars {
+  color: gold;
+}
+
+#g-rev-style-2 .tm-review-stars > i {
+  margin: 0 2px;
+}
+
+#g-rev-style-2 .tm-review-text {
+  font-style: italic;
+}
+
+</style>
+<h1>Style 2</h1>
+<div id='g-rev-style-2' uk-slider>
+
+  <div class="uk-position-relative">
+
+    <div class="uk-slider-container">
+    <ul class="uk-slider-items uk-child-width-1-1@s uk-child-width-1-4@l">
+        <li>
+          <div class="tm-review-box">
+            <span class="uk-display-block uk-text-center"><img src="https://blogsdir.imgix.net/952/files/icons/social/google.svg" alt=""></span>
+            <div class="tm-review">
+              <div class="tm-review-stars uk-text-center"><i class="uk-icon-star"></i><i class="uk-icon-star"></i><i class="uk-icon-star"></i><i class="uk-icon-star"></i><i class="uk-icon-star"></i></div>
+              <p class="tm-review-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa molestiae delectus facilis. Consequuntur cumque vero.</p>
+              <p class="tm-review-signature uk-text-center">signature</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="tm-review-box">
+            <span class="uk-display-block uk-text-center"><img src="https://blogsdir.imgix.net/952/files/icons/social/google.svg" alt=""></span>
+            <div class="tm-review">
+              <div class="tm-review-stars uk-text-center"><i class="uk-icon-star"></i><i class="uk-icon-star"></i><i class="uk-icon-star"></i><i class="uk-icon-star"></i><i class="uk-icon-star"></i></div>
+              <p class="tm-review-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa molestiae delectus facilis. Consequuntur cumque vero.</p>
+              <p class="tm-review-signature uk-text-center">signature</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="tm-review-box">
+            <span class="uk-display-block uk-text-center"><img src="https://blogsdir.imgix.net/952/files/icons/social/google.svg" alt=""></span>
+            <div class="tm-review">
+              <div class="tm-review-stars uk-text-center"><i class="uk-icon-star"></i><i class="uk-icon-star"></i><i class="uk-icon-star"></i><i class="uk-icon-star"></i><i class="uk-icon-star"></i></div>
+              <p class="tm-review-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa molestiae delectus facilis. Consequuntur cumque vero.</p>
+              <p class="tm-review-signature uk-text-center">signature</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="tm-review-box">
+            <span class="uk-display-block uk-text-center"><img src="https://blogsdir.imgix.net/952/files/icons/social/google.svg" alt=""></span>
+            <div class="tm-review">
+              <div class="tm-review-stars uk-text-center"><i class="uk-icon-star"></i><i class="uk-icon-star"></i><i class="uk-icon-star"></i><i class="uk-icon-star"></i><i class="uk-icon-star"></i></div>
+              <p class="tm-review-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa molestiae delectus facilis. Consequuntur cumque vero.</p>
+              <p class="tm-review-signature uk-text-center">signature</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="tm-review-box">
+            <span class="uk-display-block uk-text-center"><img src="https://blogsdir.imgix.net/952/files/icons/social/google.svg" alt=""></span>
+            <div class="tm-review">
+              <div class="tm-review-stars uk-text-center"><i class="uk-icon-star"></i><i class="uk-icon-star"></i><i class="uk-icon-star"></i><i class="uk-icon-star"></i><i class="uk-icon-star"></i></div>
+              <p class="tm-review-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa molestiae delectus facilis. Consequuntur cumque vero.</p>
+              <p class="tm-review-signature uk-text-center">signature</p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+
+    <a class="uk-position-center-left-out" href uk-slider-item="previous"></a>
+    <a class="uk-position-center-right-out" href uk-slider-item="next"></a>
+
+  </div>
+
+  <ul class="uk-slider-nav uk-dotnav uk-flex uk-flex-center uk-flex-middle"></ul>
+
+</div>
